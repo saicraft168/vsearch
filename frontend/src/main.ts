c30529956155c0bbed792091762db33c84bbf0db
saicraft168 <saicraft168@gmail.com>
2025-09-15 15:26:54 +0900

    Bootstrapの導入

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap'
+
 const app = createApp(App)
 
 app.use(createPinia())
