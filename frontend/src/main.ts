88ef8b9ec3df29d4cc30960dc1d559ee8d73694b
saicraft168 <saicraft168@gmail.com>
2025-09-28 22:44:23 +0900

    vue-multiselectを導入

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 /* Bootstrap カスタムSCSS */
-import "@/assets/bootstrap-custom.scss";
+import "@/assets/custom-bootstrap.scss";
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/main.css'
 
@@ -8,10 +8,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// vue-multiselect を import
+import VueMultiselect from "vue-multiselect";
+import 'vue-multiselect/dist/vue-multiselect.css'
+import '@/assets/custom-multiselect.scss'
+
 import "bootstrap/dist/js/bootstrap.bundle";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('VueMultiselect', VueMultiselect)
 app.mount('#app')
